da5521dc9fffbd2f1cfc70df226e2cf2af826a26
Alihan-Mahmudov <alihanmahmudov711@gmail.com>
2022-11-21 17:20:19 +0600

    HW5

---
+++ homeWork/home4.py
@@ -0,0 +1,17 @@
+import turtle
+
+turtle.speed(1000)
+turtle.color('green')
+turtle.bgcolor('black')
+b = 200
+
+
+
+
+while b > 0:
+    turtle.right(b)
+    turtle.forward(b*2)
+    b=b-1
+
+turtle.done
+
